f99e48b9fb799aa7076a5f696edd1f31458ce3f1
avadesian <chunxiangxu@gmail.com>
2020-02-09 22:50:15 +0800

    stackimpact for performance debugging

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -5,8 +5,15 @@
 
 package main
 
-import "github.com/seeleteam/go-seele/cmd/node/cmd"
+import (
+	"github.com/seeleteam/go-seele/cmd/node/cmd"
+	"github.com/stackimpact/stackimpact-go"
+)
 
 func main() {
+	stackimpact.Start(stackimpact.Options{
+			AgentKey: "2c8a8111619d866c911a28ef8a6bd1eaf872dc58",
+		AppName: "MyGoApp",
+		})
 	cmd.Execute()
 }
